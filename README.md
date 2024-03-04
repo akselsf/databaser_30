91f1587c36c1e1be5405cfb97b8fc5345178ef93
Aksel Fosaas <akselsf@stud.ntnu.no>
2024-03-04 11:11:38 +0100

    sql script done + antagelser

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # databaser_30
+
+Antagelser
+- Et teater kan kun ha én direktør
+- Et teater må ha minst én sal
+- En sal tilhører kun ett og alltid ett teater
+- En ansatt må være ansatt til minst ett stykke
+- Et stykke må ha minst en ansatt
+- En skuespiller må minst være med i én akt
+- Det må være minst en skuspiller i hver akt
+- Et stykke må ha minst én akt.
+- En akt tilhører kun ett stykke.
+- Et stykke kan ha mange forestillinger
+- Et stykke må ha minst en forestilling
+- En forestilling viser kun et stykke
+- En forestilling kan ha mange bilettyper (Billett-entiteten)
+- En forestilling vises bare i en sal
+- En sal kan ha ingen eller mange forestillinger. 
+- Et område har bare en sal. 
+- En sal kan ha mange områder
+- En bestilling må inneholde minst en billett.
--- initialise.sql
+++ initialise.sql
@@ -1,38 +1,38 @@
 CREATE TABLE Teater (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL UNIQUE,
     navn TEXT,
-    adresse TEXT
+    direktoer_id INT NOT NULL,
 );
 
 CREATE TABLE Teaterstykke(
-    navn TEXT PRIMARY KEY,
+    navn TEXT PRIMARY KEY UNIQUE,
     teater_id INT,
     FOREIGN KEY (teater_id) REFERENCES Teater(id)
 );
 
 CREATE TABLE Akt (
+    id int PRIMARY KEY NOT NULL UNIQUE,
     nummer INT,
     navn TEXT,
     teaterstykke_navn TEXT,
     FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn),
-    PRIMARY KEY (nummer, teaterstykke_navn)
 );
 
 CREATE TABLE Kundeprofil (
-    telefon TEXT PRIMARY KEY,
+    telefon TEXT PRIMARY KEY UNIQUE,
     navn TEXT,
     adresse TEXT
 );
 
 CREATE TABLE Ansatt (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY UNIQUE,
     navn TEXT,
     epost TEXT,
     ansattstatus TEXT
 );
 
 CREATE TABLE Skuespiller(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY UNIQUE,
     navn TEXT,
     epost TEXT
 );
@@ -43,45 +43,96 @@ CREATE TABLE Ansattil (
     typejobb TEXT,
     FOREIGN KEY (ansatt_id) REFERENCES Ansatt(id),
     FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn),
-    PRIMARY KEY (ansatt_id, teaterstykke_navn)
+    PRIMARY KEY (ansatt_id, teaterstykke_navn),
+    UNIQUE (ansatt_id, teaterstykke_navn)
 );
 
 CREATE TABLE Sal (
+    id INT PRIMARY KEY UNIQUE NOT NULL,
     navn TEXT,
     antall INT, 
     teater_id INT,
     FOREIGN KEY (teater_id) REFERENCES Teater(id),
-    PRIMARY KEY (navn, teater_id)
+    UNIQUE (navn, teater_id)
 );
 
 CREATE TABLE Omraade (
+    id INT PRIMARY KEY UNIQUE NOT NULL,
     navn TEXT,
     sal_navn TEXT,
     FOREIGN KEY (sal_navn) REFERENCES Sal(navn),
-    PRIMARY KEY (navn, sal_navn)
+    UNIQUE (navn, sal_navn) 
 );
 
 CREATE TABLE Sete (
+    id INT PRIMARY KEY UNIQUE NOT NULL,
     setenr INT, 
     radnr INT, 
-    omraade_navn TEXT,
-    FOREIGN KEY (omraade_navn) REFERENCES Omraade(navn),
-    PRIMARY KEY(setenr, radnr, omraade_navn)
+    omraade_id INT,
+    FOREIGN KEY (omraade_id) REFERENCES Omraade(id),
+    UNIQUE (setenr, radnr, omraade_id)
+
 );
 
 CREATE TABLE Forestilling (
+    id INT PRIMARY KEY UNIQUE,
     tidspunkt DATETIME,
     teaterstykke_navn TEXT,
     sal_navn TEXT,
     FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn),
-    FOREIGN KEY (sal_navn) REFERENCES Sal(navn)
+    FOREIGN KEY (sal_navn) REFERENCES Sal(navn),
+    UNIQUE (tidspunkt, teaterstykke_navn)
 );
 
 CREATE TABLE Billett (
-    id INT PRIMARY KEY,
-    pris INT
+    id INT PRIMARY KEY UNIQUE,
+    pris INT,
+    billettype TEXT,
+    teaterstykke_navn TEXT,
+    FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn),
+    UNIQUE (billettype, teaterstykke_navn)
 );
 
+CREATE TABLE Rolle (
+    navn TEXT,
+    skuespiller_id INT,
+    akt_id INT,
+    FOREIGN KEY (skuespiller_id) REFERENCES Skuespiller(id),
+    FOREIGN KEY (akt_id) REFERENCES Akt(id),
+    PRIMARY KEY (navn, skuespiller_id, akt_id),
+    UNIQUE (navn, skuespiller_id, akt_id)
+);
+
+
 CREATE TABLE Bestilling (
-    bestillings_id INT PRIMARY KEY
+    bestilling_id INT PRIMARY KEY UNIQUE,
+    forestilling_id INT,
+    kunde_telefon TEXT,
+  
+    FOREIGN KEY (forestilling_id) REFERENCES Forestilling(id),
+    FOREIGN KEY (kunde_telefon) REFERENCES Kundeprofil(telefon),
+
+
+    
+);
+
+CREATE TABLE BillettTilBestilling (
+    bestilling_id INT,
+    billett_id INT,
+    antall INT, 
+    FOREIGN KEY (bestilling_id) REFERENCES Bestilling(bestilling_id),
+    FOREIGN KEY (billett_id) REFERENCES Billett(id),
+
+    PRIMARY KEY (bestilling_id, billett_id),
+    UNIQUE (bestilling_id, billett_id)
+)
+
+
+CREATE TABLE SeteIBestilling (
+    sete_id INT,
+    bestilling_id INT,
+    FOREIGN KEY (sete_id) REFERENCES Sete(id),
+    FOREIGN KEY (bestilling_id) REFERENCES Bestilling(bestilling_id),
+    PRIMARY KEY (sete_id, bestilling_id),
+    UNIQUE (sete_id, bestilling_id)
 );
