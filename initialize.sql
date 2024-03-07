bdb4638887fcf82bc6ec82c58097a0a67a5777d5
Aksel Fosaas <akselsf@stud.ntnu.no>
2024-03-07 11:41:31 +0100

    .

--- initialize.sql
+++ initialize.sql
@@ -1,18 +1,21 @@
-CREATE TABLE Teater (
+CREATE TABLE Teater
+(
     navn TEXT NOT NULL PRIMARY KEY UNIQUE,
     direktoer_id INT NOT NULL,
     CONSTRAINT fk_direktoer_id FOREIGN KEY (direktoer_id) REFERENCES Ansatt(id)
     ON UPDATE CASCADE
 );
 
-CREATE TABLE Teaterstykke(
-     navn TEXT NOT NULL PRIMARY KEY UNIQUE,
-     teater_navn TEXT NOT NULL,
-     CONSTRAINT fk_teater_navn FOREIGN KEY (teater_navn) REFERENCES Teater(navn)
+CREATE TABLE Teaterstykke
+(
+    navn TEXT NOT NULL PRIMARY KEY UNIQUE,
+    teater_navn TEXT NOT NULL,
+    CONSTRAINT fk_teater_navn FOREIGN KEY (teater_navn) REFERENCES Teater(navn)
      ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Akt (
+CREATE TABLE Akt
+(
     nummer INT NOT NULL,
     navn TEXT,
     teaterstykke_navn TEXT NOT NULL,
@@ -23,26 +26,30 @@ CREATE TABLE Akt (
     UNIQUE (nummer, teaterstykke_navn)
 );
 
-CREATE TABLE Kundeprofil (
+CREATE TABLE Kundeprofil
+(
     telefon TEXT PRIMARY KEY UNIQUE,
     navn TEXT NOT NULL,
     adresse TEXT NOT NULL
 );
 
-CREATE TABLE Ansatt (
+CREATE TABLE Ansatt
+(
     id INTEGER PRIMARY KEY UNIQUE NOT NULL ,
     navn TEXT NOT NULL,
     epost TEXT NOT NULL UNIQUE,
-    ansattstatus TEXT NOT NULL 
+    ansattstatus TEXT NOT NULL
 );
 
-CREATE TABLE Skuespiller(
+CREATE TABLE Skuespiller
+(
     id INTEGER PRIMARY KEY UNIQUE NOT NULL ,
     navn TEXT NOT NULL,
     epost TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE AnsattTil (
+CREATE TABLE AnsattTil
+(
     ansatt_id INT NOT NULL ,
     teaterstykke_navn TEXT NOT NULL,
     typejobb TEXT NOT NULL,
@@ -54,7 +61,8 @@ CREATE TABLE AnsattTil (
 
 );
 
-CREATE TABLE Sal (
+CREATE TABLE Sal
+(
     navn TEXT NOT NULL,
     teater_navn TEXT NOT NULL,
     CONSTRAINT fk_teater_navn FOREIGN KEY (teater_navn) REFERENCES Teater(navn) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -63,7 +71,8 @@ CREATE TABLE Sal (
     UNIQUE (navn, teater_navn)
 );
 
-CREATE TABLE Omraade (
+CREATE TABLE Omraade
+(
     navn TEXT NOT NULL,
     sal_navn TEXT NOT NULL,
     teater_navn TEXT NOT NULL,
@@ -73,9 +82,10 @@ CREATE TABLE Omraade (
     UNIQUE (navn, sal_navn, teater_navn)
 );
 
-CREATE TABLE Rad (
+CREATE TABLE Rad
+(
 
-    rad_nr INT NOT NULL, 
+    rad_nr INT NOT NULL,
     omraade_navn TEXT NOT NULL,
     sal_navn TEXT NOT NULL,
     teater_navn TEXT NOT NULL,
@@ -88,10 +98,11 @@ CREATE TABLE Rad (
 
 );
 
-CREATE TABLE Sete (
+CREATE TABLE Sete
+(
 
-    sete_nr INT NOT NULL, 
-    rad_nr INT NOT NULL, 
+    sete_nr INT NOT NULL,
+    rad_nr INT NOT NULL,
     omraade_navn TEXT NOT NULL,
     sal_navn TEXT NOT NULL,
     teater_navn TEXT NOT NULL,
@@ -103,44 +114,48 @@ CREATE TABLE Sete (
 
 );
 
-CREATE TABLE Forestilling (
+CREATE TABLE Forestilling
+(
     tidspunkt DATETIME NOT NULL,
     teaterstykke_navn TEXT NOT NULL,
     sal_navn TEXT NOT NULL,
     teater_navn TEXT NOT NULL,
-    
-   CONSTRAINT fk_teaterstykke_navn FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn) ON DELETE CASCADE ON UPDATE CASCADE,
-   CONSTRAINT fk_sal_navn FOREIGN KEY (sal_navn, teater_navn) REFERENCES Sal(navn, teater_navn) ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT fk_teaterstykke_navn FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_sal_navn FOREIGN KEY (sal_navn, teater_navn) REFERENCES Sal(navn, teater_navn) ON DELETE CASCADE ON UPDATE CASCADE,
 
     PRIMARY KEY (tidspunkt, teaterstykke_navn),
     UNIQUE (tidspunkt, teaterstykke_navn)
 
 );
 
-CREATE TABLE Billett (
+CREATE TABLE Billett
+(
     pris INT NOT NULL,
     billettype TEXT NOT NULL,
     teaterstykke_navn TEXT NOT NULL,
     CONSTRAINT fk_teaterstykke_navn FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn) ON DELETE CASCADE ON UPDATE CASCADE,
-    
+
     PRIMARY KEY (billettype, teaterstykke_navn),
     UNIQUE (billettype, teaterstykke_navn)
 );
 
-CREATE TABLE Rolle (
+CREATE TABLE Rolle
+(
     navn TEXT NOT NULL,
     skuespiller_id INT NOT NULL,
     teaterstykke_navn TEXT NOT NULL,
 
     CONSTRAINT fk_teaterstykke_navn FOREIGN KEY (teaterstykke_navn) REFERENCES Teaterstykke(navn) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_skuespiller_id FOREIGN KEY (skuespiller_id) REFERENCES Skuespiller(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    
+
     PRIMARY KEY (navn, skuespiller_id, teaterstykke_navn),
     UNIQUE (navn, skuespiller_id, teaterstykke_navn)
- 
+
 );
 
-CREATE TABLE RolleIAkt (
+CREATE TABLE RolleIAkt
+(
     rolle_navn TEXT NOT NULL,
     skuespiller_id INT NOT NULL,
     teaterstykke_navn TEXT NOT NULL,
@@ -150,13 +165,14 @@ CREATE TABLE RolleIAkt (
 
     CONSTRAINT fk_rolle_key FOREIGN KEY (rolle_navn, skuespiller_id, teaterstykke_navn) REFERENCES Rolle(navn, skuespiller_id, teaterstykke_navn) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_akt_key FOREIGN KEY (akt_nummer, akt_teaterstykke_navn) REFERENCES Akt(nummer, teaterstykke_navn) ON DELETE CASCADE ON UPDATE CASCADE,
-    
+
     PRIMARY KEY (rolle_navn, skuespiller_id, teaterstykke_navn, akt_nummer, akt_teaterstykke_navn),
     UNIQUE (rolle_navn, skuespiller_id, teaterstykke_navn, akt_nummer, akt_teaterstykke_navn)
 );
 
 
-CREATE TABLE Bestilling (
+CREATE TABLE Bestilling
+(
     id INTEGER PRIMARY KEY UNIQUE NOT NULL ,
     forestilling_navn INT NOT NULL,
     forestilling_tidspunkt DATETIME NOT NULL,
@@ -169,36 +185,37 @@ CREATE TABLE Bestilling (
     CONSTRAINT fk_kunde_telefon FOREIGN KEY (kunde_telefon) REFERENCES Kundeprofil(telefon) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE BilletterTilBestilling (
+CREATE TABLE BilletterTilBestilling
+(
     bestilling_id INT NOT NULL,
 
     forestilling_navn INT NOT NULL,
     forestilling_tidspunkt DATETIME NOT NULL,
     billettype TEXT NOT NULL,
-    
-    antall INT NOT NULL CHECK (antall > 0),
+
+    antall INT NOT NULL,
     CONSTRAINT fk_forestilling_navn FOREIGN KEY (forestilling_navn, forestilling_tidspunkt) REFERENCES Forestilling(navn, tidspunkt) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_billettype FOREIGN KEY (billettype) REFERENCES Billett(billettype) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_bestilling_id FOREIGN KEY (bestilling_id) REFERENCES Bestilling(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT check_antall CHECK (antall > 0),
 
     PRIMARY KEY (bestilling_id, forestilling_navn, billettype),
     UNIQUE (bestilling_id, forestilling_navn, billettype)
 );
 
 
-CREATE TABLE SeterTilBestilling (
-    
+CREATE TABLE SeterTilBestilling
+(
+
     bestilling_id INT NOT NULL,
-    
+
     sete_nr INT NOT NULL,
     rad_nr INT NOT NULL,
     omraade_navn TEXT NOT NULL,
     sal_navn TEXT NOT NULL,
     teater_navn TEXT NOT NULL,
 
-    CONSTRAINT fk_sete_key
-        FOREIGN KEY (sete_nr, rad_nr, omraade_navn, sal_navn, teater_navn) 
-            REFERENCES Sete(sete_nr, rad_nr, omraade_navn, sal_navn, teater_navn) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_sete_key FOREIGN KEY (sete_nr, rad_nr, omraade_navn, sal_navn, teater_navn) REFERENCES Sete(sete_nr, rad_nr, omraade_navn, sal_navn, teater_navn) ON DELETE CASCADE ON UPDATE CASCADE,
 
     PRIMARY KEY (bestilling_id, sete_nr, rad_nr, omraade_navn, sal_navn, teater_navn),
     UNIQUE (bestilling_id, sete_nr, rad_nr, omraade_navn, sal_navn, teater_navn)
